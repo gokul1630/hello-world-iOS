9773b55711521bd0b71740fd89f0141c5b9f9cc9
gokul1630 <gokulgp3167@gmail.com>
2023-07-01 23:35:51 +0530

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Gokulprasanth on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
